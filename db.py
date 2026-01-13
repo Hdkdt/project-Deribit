6c983d3005a3b2330ed0e86b30e00e2fae19a58a
Hdkdt <145689787+Hdkdt@users.noreply.github.com>
2026-01-13 22:51:33 +0500

    Add SQLAlchemy setup for PostgreSQL database
    Set up SQLAlchemy engine and session for PostgreSQL database.

--- db.py
+++ db.py
@@ -1,1 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql://user:pass@localhost:5432/prices"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
