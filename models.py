ced4da7af55d3370e6830691674284b6b45948c2
Hdkdt <145689787+Hdkdt@users.noreply.github.com>
2026-01-13 22:52:34 +0500

    Add Price model for storing price data

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, Numeric, BigInteger
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Price(Base):
+    __tablename__ = "prices"
+
+    id = Column(Integer, primary_key=True)
+    ticker = Column(String)
+    price = Column(Numeric)
+    timestamp = Column(BigInteger)
 
